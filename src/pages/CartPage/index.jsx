d13b806e4992987c9e6710a9e7689d5c2934534c
Daria Lenschmidt <daria.lenshmidt@gmail.com>
2024-02-21 22:09:05 +0100

    fix path

--- src/pages/CartPage/index.jsx
+++ src/pages/CartPage/index.jsx
@@ -5,18 +5,17 @@ import {
   clearCart,
   decrementQuantity,
   deleteProduct,
-  incrementQuantity
+  incrementQuantity,
 } from "../../features/user/userSlice";
 import { ROOT_URL } from "../..";
 import { useForm } from "react-hook-form";
-import s from "./CartPage.module.css";
+import s from "./Cartpage.module.css";
 import { IoClose } from "react-icons/io5";
 import { CgClose } from "react-icons/cg";
 import AllProductsBtn from "../../components/UI/AllProductsBtn";
 import { Link } from "react-router-dom";
 
 export default function CartPage() {
-
   //form
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [showCongratulations, setShowCongratulations] = useState(false);
@@ -84,26 +83,26 @@ export default function CartPage() {
     dispatch(decrementQuantity({ id: itemId }));
   };
 
-const handleDeleteProduct = ( itemId ) => {
-  dispatch(deleteProduct({ id: itemId }))
-}
+  const handleDeleteProduct = (itemId) => {
+    dispatch(deleteProduct({ id: itemId }));
+  };
   // Общая стоимость товаров в корзине
-  const totalPrice = cart.reduce((total, item) => {
-    return (total + (item.discont_price !== null
-        ? item.discont_price * item.quantity
-        : item.price * item.quantity)
-    );
-  }, 0).toFixed(2);
+  const totalPrice = cart
+    .reduce((total, item) => {
+      return (
+        total +
+        (item.discont_price !== null
+          ? item.discont_price * item.quantity
+          : item.price * item.quantity)
+      );
+    }, 0)
+    .toFixed(2);
 
   //const roundedTotalPrice = Math.round(totalPrice * 100) / 100; // Округление до двух знаков после запятой
-
 
   const totalCount = calculateTotalCount(cart);
   console.log(cart, "cart....");
 
- 
-
- 
   // const handleDecrement = (itemId, quantity) => {
   //   // Уменьшаем количество товара на 1
   //     if (quantity > 1) {
@@ -121,39 +120,45 @@ const handleDeleteProduct = ( itemId ) => {
 
   return (
     <div className={`${s.wrapper} container`}>
-     
       {showCongratulations && (
-         <div className={s.congratulations_overlay}>
-        <div className={s.congratulations_window}>
-          <h3>Congratulations!</h3>
-          <div className={s.congratulations_text}>
-          <p>Your order has been successfully placed on the website.</p>
-          <p>A manager will contact you shortly to confirm your order.</p>
+        <div className={s.congratulations_overlay}>
+          <div className={s.congratulations_window}>
+            <h3>Congratulations!</h3>
+            <div className={s.congratulations_text}>
+              <p>Your order has been successfully placed on the website.</p>
+              <p>A manager will contact you shortly to confirm your order.</p>
+            </div>
+            <button
+              className={s.close_btn}
+              onClick={handleCloseCongratulations}
+            >
+              {" "}
+              <CgClose className={s.congratulations_icons} />
+            </button>
           </div>
-          <button className={s.close_btn} onClick={handleCloseCongratulations}> <CgClose className={s.congratulations_icons} /></button>
-        </div>
         </div>
       )}
-   <div className={s.title_btn}>
-      <h2>Shopping cart</h2>
+      <div className={s.title_btn}>
+        <h2>Shopping cart</h2>
 
-      <div className={s.category_line_container}>
-            <div className={s.categories_line}></div>
-            <Link to='/products/all'>
-              <AllProductsBtn buttonText="Back to the store" className={s.allProductBtn}/>
-            </Link>
-          </div>
+        <div className={s.category_line_container}>
+          <div className={s.categories_line}></div>
+          <Link to="/products/all">
+            <AllProductsBtn
+              buttonText="Back to the store"
+              className={s.allProductBtn}
+            />
+          </Link>
+        </div>
       </div>
       {cart.length === 0 && (
- <div className={s.continue__wrapper}>
- <p>Looks like you have no items in your basket currently.</p>
- <Link to='/'>
- <button className={s.continue__btn}>Continue Shopping</button>
- </Link>
-</div>
-
+        <div className={s.continue__wrapper}>
+          <p>Looks like you have no items in your basket currently.</p>
+          <Link to="/">
+            <button className={s.continue__btn}>Continue Shopping</button>
+          </Link>
+        </div>
       )}
-     
 
       <div className={s.wrapper__content}>
         <div className={s.container__products}>
@@ -163,47 +168,50 @@ const handleDeleteProduct = ( itemId ) => {
                 <img src={ROOT_URL + item.image} alt={item.title} />
               </div>
               <div className={s.product__info}>
-               
                 <div className={s.title}>
-                <h3 >{item.title}</h3>
-                <IoClose className={s.icons} onClick={ () => handleDeleteProduct(item.id)}/>
+                  <h3>{item.title}</h3>
+                  <IoClose
+                    className={s.icons}
+                    onClick={() => handleDeleteProduct(item.id)}
+                  />
                 </div>
-      <div className={s.count__container}>
-                <div>
-                  <div className={s.count__wrapper}>
-                    <button
-                      className={s.count_btn}
-                      onClick={() => handleDecrement(item.id, item.quantity)}
-                    >
-                      -
-                    </button>
-                    <div className={s.count}>{item.quantity}</div>
-                    <button
-                      className={s.count_btn}
-                      onClick={() => handleIncrement(item.id, item.quantity)}
-                    >
-                      +
-                    </button>
+                <div className={s.count__container}>
+                  <div>
+                    <div className={s.count__wrapper}>
+                      <button
+                        className={s.count_btn}
+                        onClick={() => handleDecrement(item.id, item.quantity)}
+                      >
+                        -
+                      </button>
+                      <div className={s.count}>{item.quantity}</div>
+                      <button
+                        className={s.count_btn}
+                        onClick={() => handleIncrement(item.id, item.quantity)}
+                      >
+                        +
+                      </button>
+                    </div>
                   </div>
-                </div>
 
-                {item.discont_price !== null && (
-                  <div className={s.price}>
-                    <span
-                      className={s.discounted_price}
-                    >{`$${item.discont_price} `}</span>
-                    <span className={s.original_price}>{`$${item.price}`}</span>
-                  </div>
-                )}
-                {item.discont_price === null && (
-                  <div className={s.price}>
-                    <span className={s.normal_price}>{`$${item.price}`}</span>
-                  </div>
-                )}
-                {/* <button onClick={() => handleAddToCart(item)}>
+                  {item.discont_price !== null && (
+                    <div className={s.price}>
+                      <span
+                        className={s.discounted_price}
+                      >{`$${item.discont_price} `}</span>
+                      <span
+                        className={s.original_price}
+                      >{`$${item.price}`}</span>
+                    </div>
+                  )}
+                  {item.discont_price === null && (
+                    <div className={s.price}>
+                      <span className={s.normal_price}>{`$${item.price}`}</span>
+                    </div>
+                  )}
+                  {/* <button onClick={() => handleAddToCart(item)}>
                   Add one more{" "}
                 </button> */}
-               
                 </div>
               </div>
             </div>
